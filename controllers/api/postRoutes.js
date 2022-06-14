7129a11722a15cf41cb806be0c21d2b687ff506b
cdfoye <cdfoye@gmail.com>
2022-06-14 15:13:12 -0600

    updated postRoutes.js to get all posts, and update a post

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,35 +1,75 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.post('/', withAuth, async (req, res) => {
+// Get ALL blog posts
+router.get('/', async (req, res) => {
   try {
-    const newProject = await Project.create({
-      ...req.body,
-      user_id: req.session.user_id,
+    const PostData = await Post.findAll({
+      order: [['date_created', 'ASC']],
     });
 
-    res.status(200).json(newProject);
+    res.status(200).json(PostData);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
+// Create a new blog post
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newPost = await Post.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// Update a blog post
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.update(
+      {
+        content: req.body.content,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Delete a blog post
 router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const projectData = await Project.destroy({
+    const postData = await Post.destroy({
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
       },
     });
 
-    if (!projectData) {
-      res.status(404).json({ message: 'No project found with this id!' });
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
       return;
     }
 
-    res.status(200).json(projectData);
+    res.status(200).json(postData);
   } catch (err) {
     res.status(500).json(err);
   }
