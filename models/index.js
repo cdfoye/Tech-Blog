46247d3c913706150ad4040f0257cb046ffcbc6b
cdfoye <cdfoye@gmail.com>
2022-06-14 14:58:28 -0600

    updated index.js with new and updated models

--- models/index.js
+++ models/index.js
@@ -1,13 +1,22 @@
 const User = require('./User');
-const Project = require('./Project');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
-User.hasMany(Project, {
+User.hasMany(Post, {
   foreignKey: 'user_id',
-  onDelete: 'CASCADE'
+  onDelete: 'CASCADE',
 });
 
-Project.belongsTo(User, {
-  foreignKey: 'user_id'
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
 });
 
 module.exports = { User, Project };
